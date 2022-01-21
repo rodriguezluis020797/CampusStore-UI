ef38531893cb01ad8a4c4eab7532243c2e006458
rodriguezluis2797 <76459394+rodriguezluis2797@users.noreply.github.com>
2022-01-21 13:58:01 -0800

    .

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,35 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/services/user-service';
+import { Cookies } from '../Cookies';
+import { LogInModel } from '../models/login-model';
 import { UserModel } from '../models/UserModel';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
-  providers:[UserService]
+  providers:[UserService, Cookies]
 })
 export class UserProfileComponent implements OnInit {
 
   constructor(
-    private users: UserService
+    private users: UserService,
+    private cookies: Cookies,
+
   ) { }
 
   public user: UserModel | undefined;
-  public providedEmail: String | undefined;
-  public providedPassword: String | undefined;
+  public credentials: LogInModel = new LogInModel();
   
   public loading: boolean = false;
 
 
   ngOnInit(): void {
 
-
+    
 
   }
 
   login() : void{
-    console.log(this.providedEmail);
-    console.log(this.providedPassword);
+    this.credentials.providedEmail = "lurodrig@linfield.edu";
+
+    this.users.getUser(this.credentials.providedEmail).subscribe(
+      (result) => {
+        this.user = result;
+        this.cookies.setUserCookie(this.user);
+        this.credentials = new LogInModel();
+      }, (error) => {
+        this.credentials.providedPassword = undefined;
+      }
+    );
   }
 
 
