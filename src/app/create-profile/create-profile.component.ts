12d2012b15268452c40876b4aa6ec15a1d748038
rodriguezluis2797 <76459394+rodriguezluis2797@users.noreply.github.com>
2022-01-21 21:59:26 -0800

    .

--- src/app/create-profile/create-profile.component.ts
+++ src/app/create-profile/create-profile.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/services/user-service';
+import { Cookies } from '../Cookies';
+import { UserModel } from '../models/UserModel';
 
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.component.html',
-  styleUrls: ['./create-profile.component.css']
+  styleUrls: ['./create-profile.component.css'],
+  providers: [UserService, Cookies]
+  
 })
 export class CreateProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private users: UserService,
+    private router: Router,
+    private cookies: Cookies
+  ) { }
+
+  public user: UserModel = new UserModel();
+  public errorMessage: string | undefined;
 
   ngOnInit(): void {
+
   }
+
+  saveNewUser() : void {
+    this.users.createUser(this.user).subscribe(
+      (result) => {
+        this.cookies.setUserCookie(result);
+        this.router.navigate(['']);
+      }, (error) => {
+        
+      }
+    );
+  }
+
+
+  cancel() : void {
+    this.router.navigate(['']);
+  }
+
+
 
 }
