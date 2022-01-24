ca09ef01d09cad69cb9dc8ac8aee26bf0789c511
rodriguezluis2797 <76459394+rodriguezluis2797@users.noreply.github.com>
2022-01-23 22:14:17 -0800

    Deleiverable

--- src/services/password-service.ts
+++ src/services/password-service.ts
@@ -8,21 +8,21 @@ import { PasswordChangeModel } from "src/app/models/password-change-model";
 
 export class PasswordService {
     readonly devUrl: string = "https://localhost:5001";
-    readonly prodUrl: string = "";
+    readonly prodUrl: string = "https://campus-store-api.azurewebsites.net";
 
     constructor(
         private http: HttpClient
     ) { }
 
     public setPassword(passwordChangeModel: PasswordChangeModel): Observable<boolean>{
-        return this.http.post<boolean>(this.devUrl+'/userpasswordhashes/setpassword', passwordChangeModel);
+        return this.http.post<boolean>(this.prodUrl+'/userpasswordhashes/setpassword', passwordChangeModel);
     }
 
     public changePassword(passwordChangeModel: PasswordChangeModel): Observable<boolean>{
-        return this.http.put<boolean>(this.devUrl+'/userpasswordhashes/changepassword', passwordChangeModel);
+        return this.http.put<boolean>(this.prodUrl+'/userpasswordhashes/changepassword', passwordChangeModel);
     }
 
     public resetPassword(eMail: string): Observable<boolean>{
-        return this.http.get<boolean>(this.devUrl+'/userpasswordhashes/resetpassword/' + eMail)
+        return this.http.get<boolean>(this.prodUrl+'/userpasswordhashes/resetpassword/' + eMail)
     }
 }
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -9,22 +9,22 @@ import { UserModel } from "src/app/models/UserModel";
 export class UserService{
 
     readonly devUrl: string = "https://localhost:5001";
-    readonly prodUrl: string = "";
+    readonly prodUrl: string = "https://campus-store-api.azurewebsites.net";
 
     constructor(
         private http: HttpClient
     ) { }
 
     public getUser(loginInfo: LogInModel): Observable<UserModel> {
-        return this.http.put<UserModel>(this.devUrl+'/users/getuser/', loginInfo);
+        return this.http.put<UserModel>(this.prodUrl+'/users/getuser/', loginInfo);
     }
 
     public updateUser(user: UserModel): Observable<UserModel> {
-        return this.http.put<UserModel>(this.devUrl+'/users/updateuser/', user);
+        return this.http.put<UserModel>(this.prodUrl+'/users/updateuser/', user);
     }
 
     public createUser(user: UserModel): Observable<UserModel>{
-        return this.http.post<UserModel>(this.devUrl+'/users/createuser/', user);
+        return this.http.post<UserModel>(this.prodUrl+'/users/createuser/', user);
     }
     
 }
